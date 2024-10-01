df45a827d45592022aea40875a3bb5b817e1212d
shubham <b200045@nitsikkim.ac.in>
2024-10-01 22:44:35 +0530

    Setup containerized mongodb server

--- server/server.js
+++ server/server.js
@@ -1,19 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-require('dotenv').config();
+require('dotenv').config({path: '../.env'});
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-// mongoose.connect(process.env.MONGO_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-// }).then(() => {
-//     console.log('MongoDB connected');
-// }).catch((err) => console.log(err));
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log('MongoDB connected');
+}).catch((err) => console.log(err));
 
 app.get('/', (req, res) => {
     res.send('Welcome to Fresh Baked Cafe API');
